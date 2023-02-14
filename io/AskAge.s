86e6627612fea85261f273571c473a19d6ee46bb
justinthelaw <justinwingchunglaw@gmail.com>
2023-02-13 21:45:03 -0800

    offset register

--- io/AskAge.s
+++ io/AskAge.s
@@ -2,20 +2,18 @@
 # Author: Justin Law
 # Date: 13 Feb 2023
 # Purpose: this program asks the user for their age and outputs it
-
 .text
 .global main
 
 main:
+  # push the stack
   # create space on the stack by subtracting 4 bytes from the stack pointer
   SUB sp, sp, #4
+  # save a register called link register to that point on the stack at offset of 0
+  STR lr, [sp, #0]
 
-  # prompt the user for their age
+  # print the prompt for the user
   LDR r0, =promptAge
-  BL printf
-
-  # print out the format string
-  LDR r0, =formatAge
   BL printf
 
   # read in the user's age
@@ -28,12 +26,15 @@ main:
   LDR r1, [sp, #0]
   BL printf
 
-  # clean up the stack and return
+  # pop the stack and return
+  # retrieve the link register from the stack
+  LDR lr, [sp, #0]
+  # add 4 bytes back to stack
   ADD sp, sp, #4
-  MOV r0, #0
-  BX lr
+  # return, move link register back to PC
+  MOV pc, lr
 
 .data
   promptAge: .asciz "Enter your age: "
   outputAge: .asciz "Your age is: %d\n"
-  formatAge: .asciz "%d\n"
+  formatAge: .asciz "%d"
