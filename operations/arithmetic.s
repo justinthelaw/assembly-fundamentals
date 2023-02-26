4fc92ae0f9b4b871e621a587d0099f865cb34e1a
justinthelaw <justinthelaw@gmail.com>
2023-02-26 02:08:11 +0000

    improved div comments, gitignore compiled files

--- operations/arithmetic.s
+++ operations/arithmetic.s
@@ -2,34 +2,39 @@
 # Program name: arithmetic.s
 # Author: Justin Law
 # Date: 25 Feb 2023
-# Purpose: this is a program to test simple arithmetic using 3-address instructions
+# Purpose: This program demonstrates simple arithmetic using 3-address instructions.
 #
 
 .text
 .global main
 
 main:
-  # Save return to OS stack
+  # Save return address to OS stack
+  # Allocate 4 bytes of space on the stack for return address
   SUB sp, sp, #4
+  # Store the return address to the top of the stack
   STR lr, [sp]
   
   ## Divide 8 by 2
-  # move the immediate decimal 8 into register 0
+  # Move the decimal value 8 into register 0
   MOV r0, #8
-  # move the immediate decimal 2 into register 1
+  # Move the decimal value 2 into register 1
   MOV r1, #2
-  # branch link division function, r0 / r1 values (8 / 2)
+  # Call the integer division function, which computes r0 / r1 (8 / 2)
   BL __aeabi_idiv
-  # move value in register 1 to register 0
+  # Move the result from register r0 to register r1
   MOV r1, r0
-  # load into register 0 the output data format
+  # Load the output format string into register r0
   LDR r0, =div
-  # branch link C printf function
+  # Call the C printf function with the output format string and the result in register r1
   BL printf
 
   # Return to OS
+  # Restore the return address from the stack to the link register
   LDR lr, [sp]
+  # Deallocate the space used by the return address on the stack
   ADD sp, sp, #4
+  # Return to the caller by branching to the address in the link register
   MOV pc, lr
 
 .data
