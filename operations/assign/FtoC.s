3c03ed2884293f4baec12e09db75eb9c6071b073
justinthelaw <justinwingchunglaw@gmail.com>
2023-02-25 21:58:11 -0800

    FtoC mod4

--- operations/assign/FtoC.s
+++ operations/assign/FtoC.s
@@ -31,17 +31,17 @@ main:
   BL scanf
 
   ## Perform multiplication first
-  ADD r0, [sp, #0]
+  LDR r0, [sp]
   LDR r1, #9
   MUL r0, r0, r1
 
   ## Perform division next
-  ADD r0, [sp, #0]
+  LDR r0, [sp]
   MOV r1, #5
   BL __aeabi_idiv
 
   ## Perform addition last
-  ADD r0, [sp, #0]
+  LDR r0, [sp]
   ADD r0, r0, #32
   MOV r1, r0
 
