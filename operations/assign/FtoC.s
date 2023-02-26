9b5322a3ef4d5f0faa70ad9bcda9b1b63a21309f
justinthelaw <justinwingchunglaw@gmail.com>
2023-02-25 23:22:02 -0800

    FtoC mod27

--- operations/assign/FtoC.s
+++ operations/assign/FtoC.s
@@ -32,16 +32,16 @@ main:
   ## Conversion
   # Subtract 32 from the input temperature
   MOV r2, #32
-  SUB r0, r0, r2
+  SUB r1, r1, r2
   # Multiply the result by 5
   MOV r2, #5
-  MUL r0, r0, r2
+  MUL r1, r1, r2
   # Divide the result by 9 using division function
-  MOV r1, r0
+  MOV r0, r1
   MOV r0, #9
   BL __aeabi_idiv
   # Move the result of the division back into r1
-  MOV r1, r0
+  @ MOV r1, r0
 
   ## Print out the resultant temperature
   # Load into register r0 the output format
