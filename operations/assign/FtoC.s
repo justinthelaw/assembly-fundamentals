05e0618e920eaf6c02d7465d348946b6ac224b12
justinthelaw <justinwingchunglaw@gmail.com>
2023-02-25 22:23:27 -0800

    FtoC mod16

--- operations/assign/FtoC.s
+++ operations/assign/FtoC.s
@@ -25,8 +25,6 @@ main:
   ## Scan the user's input temperature into memory
   # Load into register r0 the input temperature format
   LDR r0, =formatTemp
-  # Reset register r0 with value 0
-  MOV r1, #0
   # Branch and link to C's scanf function
   BL scanf
 
